1106b17a6402fa91d66b4844ea50c9f9c7a2dda6
Niels Loew <niels@nielsloew.de>
2021-04-14 00:20:16 +0200

    update

--- SOWL.js
+++ SOWL.js
@@ -1,4 +1,5 @@
 class SOWL_System {
+    //CONFIG.debug.hooks = true;
     constructor() {
         this._puzzleController = null;
         this._animationStack = new Object();
@@ -18,6 +19,104 @@ class SOWL_System {
     _arrayRemove(arr, value) {
         return arr.filter(function (ele) {
             return ele != value;
+        });
+    }
+
+    _testForLight(point, { tolerance = 2, object = null } = {}) {
+        // Determine the array of offset points to test
+        var tolerance = Math.min(point.w, point.h) / 4;
+        const t = tolerance;
+        //point = point.center
+
+        //console.log(point)
+
+        const offsets = t > 0 ? [[0, 0], [-t, 0], [t, 0], [0, -t], [0, t], [-t, -t], [-t, t], [t, t], [t, -t]] : [[0, 0]];
+        const points = offsets.map(o => new PIXI.Point(point.x + o[0], point.y + o[1]));
+        //console.log(points);
+        var lightOnToken = [];
+
+        // Logging map object to console
+        canvas.lighting.sources.forEach((value, key) => {
+            //console.log(value + " = " + key);
+            var isHit = points.some(p => value.illumination.fov.geometry.containsPoint(p))
+            //console.log(isHit);
+            if (isHit) {
+                var hitter = key.split(".")[1]
+                //console.log(hitter)
+                //lightOnToken.push(key.split(".")[1])
+                lightOnToken.push(canvas.tokens.placeables.filter(x => x.data._id.includes(hitter))[0]);
+            }
+        })
+
+        if (lightOnToken.length > 0) {
+            return lightOnToken;
+        } else {
+            return false;
+        }
+    }
+
+    _testForLightBy(point, listOfSources) {
+        // Determine the array of offset points to test
+        var tolerance = Math.min(point.w, point.h) / 4;
+        const t = tolerance;
+        //point = point.center
+
+        //console.log(point)
+
+        const offsets = t > 0 ? [[0, 0], [-t, 0], [t, 0], [0, -t], [0, t], [-t, -t], [-t, t], [t, t], [t, -t]] : [[0, 0]];
+        const points = offsets.map(o => new PIXI.Point(point.x + o[0], point.y + o[1]));
+        //console.log(points);
+        var lightOnToken = [];
+
+        //console.log("SOURCES")
+        //console.log(listOfSources)
+        //console.log("------")
+
+        // Logging map object to console
+        listOfSources.forEach((value, key) => {
+            //console.log(value + " = " + key);
+            var isHit = points.some(p => value.light.illumination.fov.geometry.containsPoint(p))
+            //console.log(isHit);
+            if (isHit) {
+                //var hitter = value
+                console.log("is hit by")
+                console.log(value.name)
+                //lightOnToken.push(key.split(".")[1])
+                lightOnToken.push(value);
+            }
+        })
+
+        //console.log(lightOnToken)
+        if (lightOnToken.length > 0) {
+            return lightOnToken;
+        } else {
+            return false;
+        }
+    }
+
+    _copyLightProperties(lightSource, target) {
+        //console.log(lightSource[0].data)
+        var light = lightSource[0].data;
+        target.update({
+            //vision: light.vision,
+            dimSight: light.dimSight,
+            brightSight: light.brightSight,
+            dimLight: light.dimLight,
+            brightLight: light.brightLight,
+            lightAngle: light.lightAngle,
+            lockRotation: light.lockRotation,
+            lightColor: light.lightColor,
+            lightAlpha: light.lightAlpha
+        });
+    }
+
+    _disableLight(target) {
+        //console.log("delete light from")
+        //console.log(target)
+        target.update({
+            //vision: light.vision,
+            dimLight: 0,
+            brightLight: 0
         });
     }
 
@@ -188,5 +287,146 @@ class SOWL_RingPuzzle1 {
 
 }
 
+class SOWL_MirrorPuzzle1 {
+    constructor() {
+        Hooks.on("sightRefresh", this._calculateMirrors.bind(this));
+        //        Hooks.on("sightRefresh", this._calculateReceiver.bind(this));
+        Hooks.on("lightingRefresh", this._calculateMirrors.bind(this));
+        //        Hooks.on("lightingRefresh", this._calculateReceiver.bind(this));
+        this.SOWL_Mirrors = new Array();
+        this.SOWL_Receiver = new Array();
+        this.SOWL_Emitter = new Array();
+    };
+
+    _addMirror(token) {
+        this.SOWL_Mirrors.push(token)
+    }
+
+    _deleteMirror(token) {
+
+    }
+
+    _addReceiver(token) {
+        this.SOWL_Receiver.push(token)
+    }
+
+    _deleteReceiver(token) {
+
+    }
+
+    _addEmitter(token) {
+        this.SOWL_Emitter.push(token)
+    }
+
+    _deleteEmitter(token) {
+
+    }
+
+    /*
+        _calculateMirrors() {
+            console.log("Calculating Mirrors")
+            //console.log(this.SOWL_Mirrors)
+            var mirrors = this.SOWL_Mirrors
+            if (mirrors.length == 0) {
+                return;
+            }
+            //console.log(mirrors)
+            for (var key in mirrors) {
+                //console.log(typeof (mirrors[key]))
+    
+                if (typeof (mirrors[key]) == "object") {
+                    var islit = game.SOWL._testForLight(mirrors[key].center);
+                    //console.log(mirrors[key].center)
+                    if (islit) {
+                        console.log("activate mirror")
+                        game.SOWL._copyLightProperties(islit, mirrors[key])
+                    } else if (mirrors[key].emitsLight) {
+                        console.log("deactivate mirror")
+                        //console.log(mirror)
+                        game.SOWL._disableLight(mirrors[key]);
+                    }
+                }
+    
+            }
+            this._calculateReceiver()
+        }
+    */
+
+    _calculateMirrors() {
+        console.log("Calculating Mirrors")
+        //console.log(this.SOWL_Mirrors)
+        var mirrors = this.SOWL_Mirrors
+        if (mirrors.length == 0) {
+            return;
+        }
+        //console.log(mirrors)
+        for (var key in mirrors) {
+            //console.log(typeof (mirrors[key]))
+
+            if (typeof (mirrors[key]) == "object") {
+                //var islit = game.SOWL._testForLight(mirrors[key].center);
+                var islit = game.SOWL._testForLightBy(mirrors[key].center, this.SOWL_Emitter);
+                //console.log(mirrors[key].center)
+                if (islit) {
+                    console.log("activate mirror")
+                    game.SOWL._copyLightProperties(islit, mirrors[key])
+                } else if (mirrors[key].emitsLight) {
+                    console.log("------------>deactivate mirror")
+                    //console.log(mirror)
+                    game.SOWL._disableLight(mirrors[key]);
+                }
+            }
+
+        }
+        this._calculateReceiver()
+    }
+
+    _activateReceiver(target) {
+        //console.log(lightSource[0].data)
+        target.update({
+            //vision: light.vision,
+            dimLight: 2,
+            brightLight: 0
+        });
+    }
+
+    _calculateReceiver() {
+        console.log("Calculating Receivers")
+        //console.log(this.SOWL_Mirrors)
+        var receiver = this.SOWL_Receiver
+        if (receiver.length == 0) {
+            return;
+        }
+        //console.log(mirrors)
+        for (var key in receiver) {
+            //console.log(typeof (receiver[key]))
+
+            if (typeof (receiver[key]) == "object") {
+                var islit = game.SOWL._testForLightBy(receiver[key].center, this.SOWL_Mirrors);
+                //console.log(mirrors[key].center)
+                //console.log(receiver[key])
+                if (islit) {
+                    console.log("activate receiver")
+                    this._activateReceiver(receiver[key])
+                } else if (receiver[key].emitsLight) {
+                    console.log("------------->deactivate receiver")
+                    game.SOWL._disableLight(receiver[key]);
+                }
+            }
+
+        }
+    }
+
+
+    _SOWL_getLightSourcesOnMe(token) {
+        tokenIsLit = token._testForLight()
+        if (tokenIsLit) {
+
+        }
+    }
+
+}
+
 Hooks.on('init', () => game.SOWL = new SOWL_System());
 Hooks.on('init', () => game.SOWL_RingPuzzle1 = new SOWL_RingPuzzle1());
+Hooks.on('init', () => game.SOWL_MirrorPuzzle1 = new SOWL_MirrorPuzzle1());
